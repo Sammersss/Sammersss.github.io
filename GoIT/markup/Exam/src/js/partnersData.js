0a7b71616d1405e1ac29b027bb88491200f01641
Sammersss <alex-s1986@ya.ru>
2016-03-27 20:26:13 +0300

    fix

--- GoIT/markup/Exam/src/js/partnersData.js
+++ GoIT/markup/Exam/src/js/partnersData.js
@@ -2,23 +2,23 @@
 
 var partners = {
 	'1': {
-		'photo': 'dist/img/partner1.jpg',
+		'photo': 'img/partner1.jpg',
 		'name':  'Bradley Hunter',
 		'hobby': 'Based in Chicago. I love playing tennis and loud music.'
 	},
 	'2': {
-		'photo': 'dist/img/partner2.jpg',
+		'photo': 'img/partner2.jpg',
 		'name':  'Heather Walker',
 		'hobby': 'I\'m a happy person that loves cats and climbing on mountains.'
 
 	},
 	'3':{
-		'photo': 'dist/img/partner3.jpg',
+		'photo': 'img/partner3.jpg',
 		'name':  'Lucas Marsha',
 		'hobby': 'I get my inspiration from nature and objects around me. I have a passion to colours, typography and skateboards.'
 	},
 	'4':{
-		'photo': 'dist/img/partner4.jpg',
+		'photo': 'img/partner4.jpg',
 		'name':  'Bradley Hunter',
 		'hobby': 'Based in Chicago. I love playing tennis and loud music.'
 	}
