20bbe85c80c26c59614489e9cd41e8821661a833
Sammersss <alex-s1986@ya.ru>
2016-08-14 22:07:09 +0300

    rebuild project

---
+++ Demydiv/src/js/main.js
@@ -0,0 +1,15 @@
+/*
+* Third party
+*/
+//= ../../bower_components/jquery/dist/jquery.js
+
+
+/*
+* Custom
+*/
+//= partials/clock.js
+//= partials/menu.js
+//= partials/resize.js
+//= partials/classie.js
+//= partials/nav.js
+
---
+++ Demydiv/src/js/partials/menu.js
@@ -0,0 +1,35 @@
+$(function($) {
+
+//	add class active to menu link
+	var $menuLink = $('.menu__link');
+	$menuLink.on('click', function(e) {
+		e.preventDefault();
+		$(this)
+			.addClass('active')
+
+			.parent().siblings().find('.active')
+			.removeClass('active');
+		console.log('this:', this);
+	});
+
+	//	 smooth scrolling
+//	$('a[href^="#"]').bind('click.smoothscroll',function (e) {
+//		e.preventDefault();
+//
+//		var target = this.hash,
+//			$target = $(target);
+//
+//		$('html, body').stop().animate({
+//			'scrollTop': $target.offset().top
+//		}, 900, 'swing', function () {
+//			window.location.hash = target;
+//		});
+//	});
+
+	//back to Home
+//	$(window).scroll(function () {if ($(this).scrollTop() > 0) {$('#scroller').fadeIn();} else {$('#scroller').fadeOut();}});
+//	$('#scroller').click(function () {$('body,html').animate({scrollTop: 0}, 400); return false;});
+//
+
+
+});
