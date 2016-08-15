d16833b735b9faa0117c749e48fedd4ba30e8cb0
Sammersss <alex-s1986@ya.ru>
2016-08-15 22:29:32 +0300

    add weather

--- Demydiv/src/js/partials/resize.js
+++ Demydiv/src/js/partials/resize.js
@@ -1,10 +1,10 @@
-$(document).ready(function() {        
+$(document).ready(function() {
 
-    $('.resize').width($('.wrapper').width());
-    $('.header').height($('.header').width()/3.41333333333333);
-  
-    $(window).resize(function() {
-        $('.resize').width($('.wrapper').width());
-		$('.header').height($('.header').width()/3.41333333333333);
-         })
-    });
+	$('.resize').width($('.wrapper').width()+1);
+	$('.header').height($('.header').width()/3.413333333333333);
+
+	$(window).resize(function() {
+		$('.resize').width($('.wrapper').width());
+		$('.header').height($('.header').width()/3.413333333333333);
+		 })
+	});
